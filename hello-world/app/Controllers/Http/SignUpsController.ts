eb7c2924a0f87885b01fb51422715fcab91ad54a
ImNotMenduina <mynameis_menduina@outlook.com>
2023-01-18 21:02:00 -0300

    Hash e sql

--- hello-world/app/Controllers/Http/SignUpsController.ts
+++ hello-world/app/Controllers/Http/SignUpsController.ts
@@ -1,6 +1,7 @@
 //import { Response } from '@adonisjs/core/build/standalone'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import User from 'App/Models/User' //importante importar o model, pois ele cuidadará de todas as queryies dentro da table no banco de dados 
 
 
 export default class SignUpsController {
@@ -18,9 +19,18 @@ export default class SignUpsController {
                 [
                     rules.confirmed('password_confirmation')]) , 
             password_confirmation : schema.string()
+
         })
 
-        const payload = await request.validate({schema : newPostSchema}) 
+        const payload = await request.validate({schema : newPostSchema , 
+        
+        messages : {
+                'name.required' : 'Insira seu nome' , 
+                'email.required' : 'Insira um e-mail válido' , 
+                'password.required' : 'Senhas não coincidem' , 
+                'password_confirmation.required' : 'Senhas não coincidem'
+        }
+        })
        /*  try
         {
             const payload = await request.validate({schema : newPostSchema}) 
@@ -30,6 +40,13 @@ export default class SignUpsController {
             response.badRequest(error.messages)
         } 
          */
-        
+
+        const user = new User()
+        user.name = payload.name 
+        user.email = payload.email 
+        user.password = payload.password
+        await user.save() 
+
+        return response.redirect('/') 
     }
 }
