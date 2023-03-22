f284fbd37fa3c99d3d7bec5b610be0e8de471d6e
ImNotMenduina <mynameis_menduina@outlook.com>
2023-03-22 12:41:18 -0300

    Controller Validation

--- CRUD_Testes/crud/app/Controllers/Http/SignupController.ts
+++ CRUD_Testes/crud/app/Controllers/Http/SignupController.ts
@@ -1,8 +1,46 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
 
 export default class SignupController {
-    public async cadastro(ctx : HttpContextContract)
+    public async cadastro({request , response} : HttpContextContract)
     {
-      return ctx
+
+      const my_schema = schema.create({
+        nome : schema.string([rules.minLength(3) , rules.trim() ]) ,
+        sobrenome : schema.string([rules.minLength(3)]) ,
+        idade : schema.number([ rules.unsigned() , rules.range(18 , 110) ]) ,
+        email : schema.string([ rules.email() , rules.unique({ table: 'users' , column: 'email'}) ]) ,
+        password_1 : schema.string([ rules.confirmed('password_2') ]) ,
+      })
+
+      try{
+
+        //validação body contra o schema criado anteriormente.
+        //(Faz o match)
+        const validation = await request.validate({
+          schema : my_schema
+        })
+
+        //instancio o Model, atribui para cada atributo
+        //o valor validado anteriormente
+
+        const user = new User
+        user.nome = validation.nome
+        user.sobrenome = validation.sobrenome
+        user.idade = validation.idade
+        user.email = validation.email
+        user.password = validation.password_1
+
+        //salva os dados no banco SQLite
+        await user.save()
+
+      }
+      catch(e)
+      {
+        response.badRequest(e.messages)
+      }
+
     }
 }
