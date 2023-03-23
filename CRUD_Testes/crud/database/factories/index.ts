8fe8f890237bf4eec57fab26ced02f8253a08557
ImNotMenduina <mynameis_menduina@outlook.com>
2023-03-23 12:51:29 -0300

    japa

--- CRUD_Testes/crud/database/factories/index.ts
+++ CRUD_Testes/crud/database/factories/index.ts
@@ -1,5 +1,7 @@
 import Factory from '@ioc:Adonis/Lucid/Factory'
 import User from 'App/Models/User'
+import { assert } from '@japa/assert'
+import { test } from '@japa/runner'
 
 export const UserFactory = Factory
   .define(User, ({ faker }) => {
